1f766ccae27f3152ee378f155c672de61c654d63
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-05-11 22:32:01 +0200

    Add toaster lib

--- src/app/(pages)/dashboard/layout.tsx
+++ src/app/(pages)/dashboard/layout.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Toaster } from "react-hot-toast";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathName = usePathname();
@@ -20,49 +21,53 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   ];
 
   return (
-    <div className="w-full">
-      <div className="drawer lg:drawer-open">
-        <div className="drawer-content flex flex-col py-10 px-6 md:px-12">
-          <label htmlFor="navigation-drawer" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-          {children}
-        </div>
+    <>
+      <Toaster position="top-right" />
 
-        <input id="navigation-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="w-full">
+        <div className="drawer lg:drawer-open">
+          <div className="drawer-content flex flex-col py-10 px-6 md:px-12">
+            <label htmlFor="navigation-drawer" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+            {children}
+          </div>
 
-        <div className="drawer-side lg:p-10">
-          <label htmlFor="navigation-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <input id="navigation-drawer" type="checkbox" className="drawer-toggle" />
 
-          <div className="h-full p-4 bg-white lg:rounded-xl lg:shadow-lg border overflow-auto flex flex-col">
-            <div className="mb-4">
-              <Link href="/" className="btn btn-ghost h-auto flex flex-col p-4">
-                <img src="/favicon.ico" alt="Airneis" className="w-28" />
-                <p className="m-0 text-xl">Airneis</p>
-              </Link>
-            </div>
+          <div className="drawer-side lg:p-10">
+            <label htmlFor="navigation-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
 
-            <ul className="menu p-0 min-w-64 text-base-content flex-1">
-              {/* Sidebar content here */}
-              {menuItems.map((item, index) => {
-                if (!item) return <li key={index}></li>;
+            <div className="h-full p-4 bg-white lg:rounded-xl lg:shadow-lg border overflow-auto flex flex-col">
+              <div className="mb-4">
+                <Link href="/" className="btn btn-ghost h-auto flex flex-col p-4">
+                  <img src="/favicon.ico" alt="Airneis" className="w-28" />
+                  <p className="m-0 text-xl">Airneis</p>
+                </Link>
+              </div>
 
-                const isActive = item.url === "/dashboard" ? pathName === item.url : pathName.startsWith(item.url);
+              <ul className="menu p-0 min-w-64 text-base-content flex-1">
+                {/* Sidebar content here */}
+                {menuItems.map((item, index) => {
+                  if (!item) return <li key={index}></li>;
 
-                return (
-                  <li key={index}>
-                    <Link href={item.url} className={isActive ? "active" : ""}>
-                      <i className={"fa-solid " + item.icon + " me-2"}></i>{item.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+                  const isActive = item.url === "/dashboard" ? pathName === item.url : pathName.startsWith(item.url);
 
-            <div className="mt-8">
-              <Link href="/" className="btn btn-sm btn-ghost mx-1 w-full justify-start"><i className="fa-solid fa-right-from-bracket me-2"></i>Retour au site</Link>
+                  return (
+                    <li key={index}>
+                      <Link href={item.url} className={isActive ? "active" : ""}>
+                        <i className={"fa-solid " + item.icon + " me-2"}></i>{item.title}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+
+              <div className="mt-8">
+                <Link href="/" className="btn btn-sm btn-ghost mx-1 w-full justify-start"><i className="fa-solid fa-right-from-bracket me-2"></i>Retour au site</Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
